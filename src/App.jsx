b654e4f1796d84b8e63d49a48fd45ec13c2ade94
Benny Chrispin <ndizibaidu23@gmail.com>
2023-11-28 21:51:19 +0200

    Create pages and routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,19 @@
+import { Routes, Route, BrowserRouter } from "react-router-dom";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Signin from "./pages/Signin";
+import Profile from "./pages/Profile";
+import Singout from "./pages/Singout";
 export default function App() {
-  return <h1 className="text-red-500">App</h1>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/signin" element={<Signin />} />
+        <Route path="/singout" element={<Singout />} />
+        <Route path="/profile" element={<Profile />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
---
+++ src/pages/Singout.jsx
@@ -0,0 +1,3 @@
+export default function Singout() {
+  return <div>Singout</div>;
+}
