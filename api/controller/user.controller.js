4af8da6f159b277d7f81e7d2838c6eeecb97edbf
Benny Chrispin <ndizibaidu23@gmail.com>
2023-12-08 10:00:04 +0200

    create user update api router

--- api/controller/user.controller.js
+++ api/controller/user.controller.js
@@ -1,5 +1,42 @@
+import bcryptjs from "bcryptjs";
+import User from "../models/User.js";
+
+// Define errorHandler function
+const errorHandler = (code, message) => {
+  const error = new Error(message);
+  error.statusCode = code;
+  return error;
+};
+
 export const test = (req, res) => {
   res.json({
-    message: "Hello World!",
+    message: "Api route is working",
   });
+};
+
+export const updateUser = async (req, res, next) => {
+  if (req.user.id !== req.params.id)
+    return next(errorHandler(4001, "You can only update your own account!"));
+
+  try {
+    if (req.body.password) {
+      req.body.password = bcryptjs.hashSync(req.body.password, 10);
+    }
+    const updateUser = await User.findByIdAndUpdate(
+      req.params.id,
+      {
+        $set: {
+          username: req.body.name,
+          email: req.body.email,
+          password: req.body.password,
+          avatar: req.body.avatar,
+        },
+      },
+      { new: true }
+    );
+    const { password, ...rest } = updateUser._doc;
+    res.status(200).json(rest);
+  } catch (error) {
+    next(error);
+  }
 };
