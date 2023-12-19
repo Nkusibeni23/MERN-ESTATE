19b856160597261b2c9e25bb169389e078a6dcec
Benny Chrispin <ndizibaidu23@gmail.com>
2023-12-19 18:15:59 +0200

    Add onChange and onSubmit  to the search page

--- api/controller/listing.controller.js
+++ api/controller/listing.controller.js
@@ -94,7 +94,7 @@ export const getListings = async (req, res, next) => {
     const order = req.query.order || "desc";
 
     const listings = await Listing.find({
-      name: { $regex: searchTerm, $option: "i" },
+      name: { $regex: searchTerm, $options: "i" },
       offer,
       furnished,
       parking,
